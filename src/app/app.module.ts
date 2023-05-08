2e804d3d5cadcbdfd28618ea1d7e133c1ed97e86
akashmartule <akashmartule27@gmail.com>
2023-05-08 20:57:14 +0530

    added data summary component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataSummaryComponent } from './data-summary/data-summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataSummaryComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data-summary/data-summary.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DataSummaryComponent } from './data-summary.component';
+
+describe('DataSummaryComponent', () => {
+  let component: DataSummaryComponent;
+  let fixture: ComponentFixture<DataSummaryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DataSummaryComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DataSummaryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
