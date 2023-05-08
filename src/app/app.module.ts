aaf16c3862fdde5c1cfc937919b66af32a23684a
akashmartule <akashmartule27@gmail.com>
2023-05-08 21:32:00 +0530

    Added Data selector component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatDialogModule } from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataSelectorPageComponent } from './data-selector-page/data-selector-page.component';
+import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {  HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataSelectorPageComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule ,
+    FormsModule,
+    MatIconModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
