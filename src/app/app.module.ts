dc0e9bd1c9d5ec7ab83b72c623bd4ba9f0032e2e
OmMY692 <70805816+OmMY692@users.noreply.github.com>
2023-05-08 20:11:15 +0530

    added multiple components
    added report-page, add-button, footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReportPageComponent } from './report-page/report-page.component';
+import { AddButtonComponent } from './add-button/add-button.component';
+import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReportPageComponent,
+    AddButtonComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
