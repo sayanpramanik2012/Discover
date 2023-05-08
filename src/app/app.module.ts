22931459ff2c1e861c31202b41714d51c67debe3
SAYAN PRAMANIK <41908711+sayanpramanik2012@users.noreply.github.com>
2023-05-08 18:58:33 +0530

    Revert "Brand bar full GUI"
    This reverts commit ae6e03d56398d1afdc26824c7d57c6388a1f7d63.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrandBarComponent } from './brand-bar/brand-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BrandBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatIconModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/brand-bar/navigation-items.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum NavigationItem {
-  Mb = 'Monitor my business',
-  Ct = 'Choose a template',
-  Bt = 'Build a table',
-  Fs = 'Find my stuff'
-}
