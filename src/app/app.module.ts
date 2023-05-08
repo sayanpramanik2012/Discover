ae6e03d56398d1afdc26824c7d57c6388a1f7d63
SAYAN PRAMANIK <41908711+sayanpramanik2012@users.noreply.github.com>
2023-05-08 18:53:27 +0530

    Brand bar full GUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandBarComponent } from './brand-bar/brand-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
