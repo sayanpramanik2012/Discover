9eba6793df27fe85b75c5c040f4d9907933997d5
SAYAN PRAMANIK <41908711+sayanpramanik2012@users.noreply.github.com>
2023-05-09 10:47:16 +0530

    Sidepanel added with simple css

---
+++ src/app/side-panel/side-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-side-panel',
+  templateUrl: './side-panel.component.html',
+  styleUrls: ['./side-panel.component.scss']
+})
+export class SidePanelComponent{
+
+}
