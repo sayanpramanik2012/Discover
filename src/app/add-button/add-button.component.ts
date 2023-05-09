823934bd79f3eab8932e9aac702b73424e8ecac0
OmMY692 <70805816+OmMY692@users.noreply.github.com>
2023-05-09 15:42:02 +0530

    added add-button component

--- src/app/add-button/add-button.component.ts
+++ src/app/add-button/add-button.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { of, delay } from 'rxjs';
 
 @Component({
   selector: 'app-add-button',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-button.component.scss']
 })
 export class AddButtonComponent {
+  isDropdownOpen = false;
+  displayTable = false ;
+  // showRunButton=false;
+  
+  @Output() statusTableDisplay = new EventEmitter<boolean>();
+  @Output() statusRunButtonDisplay = new EventEmitter<boolean>();
+  buttons = [
+    { name: 'Table', icon: 'fa fa-table' },
+    { name: 'Line Chart', icon: 'fa fa-line-chart' },
+  ];
+  handleButtonClick(button: string) {
+    switch (button) {
+      case 'Table':
+        // Handle table button click
+        this.showTable();
+        this.toggleDropdown();
+        break;
+      case 'Line Chart':
+        // Handle line chart button click
+        
+        this.toggleDropdown();
+        break;
+      default:
+        break;
+    }
+  }
 
+  toggleDropdown() {
+    if (this.isDropdownOpen) {
+      this.isDropdownOpen = false;
+    } else {
+      this.isDropdownOpen = true;
+    }
+  }
+  showTable(){
+
+    
+      this.displayTable = true;
+      this.statusTableDisplay.emit(this.displayTable);
+      this.statusRunButtonDisplay.emit(true);
+      // this.showRunButton = true;
+    
+    // this.displayTable= true;
+    // this.statusTableDisplay.emit(this.displayTable)
+  }
 }
