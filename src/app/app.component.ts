53014450680d3443a0ec1ab2848018e3440bc551
SAYAN PRAMANIK <41908711+sayanpramanik2012@users.noreply.github.com>
2023-05-11 10:39:46 +0530

    migrated 2 component
    migrated data-summary and side-panel to report page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,38 +11,8 @@ export class AppComponent {
   title = 'Project_demo';
   buttonVisible = true;
   dataSelected = false;
-  openSidePanel = false;
-  clickedLabel = '';
   width =0;
 
-  sidePanelWidth(sidePanel:number):void {
-    this.width=sidePanel;
-  }
-
-  showPanel(label: string): void {
-    this.clickedLabel = label;
-    this.openSidePanel = true;
-  }
-  onLabelClick(label: string): void {
-    this.clickedLabel = label;
-  }
-  // isPanelOpen = true;
-  singleVal="many";
-  handleValueSelected(value: string): void {
-    this.singleVal =value;
-    console.log("Selected value:", this.singleVal);
-  }
-
-  onPanelClosed(): void {
-    this.openSidePanel = false;
-    console.log("close triggered");
-  }
-  count = 0;
-
-updateCount(newCount: number): void {
-  this.count = newCount;
-  console.log("appsidebar",this.count);
-}
 onUpdate(): void {
 
 }
