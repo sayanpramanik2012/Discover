292d1f2353123c6e75c6e9218bbe1ebaa5954b02
SAYAN PRAMANIK <41908711+sayanpramanik2012@users.noreply.github.com>
2023-05-12 21:15:53 +0530

    Line chart added to report page

--- src/app/add-button/add-button.component.ts
+++ src/app/add-button/add-button.component.ts
@@ -1,18 +1,18 @@
-import { Component, EventEmitter,Input,Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ShimmerService } from '../services/shimmer.service';
 import { delay } from 'rxjs/operators';
 import { of } from 'rxjs';
 @Component({
   selector: 'app-add-button',
   templateUrl: './add-button.component.html',
-  styleUrls: ['./add-button.component.scss']
+  styleUrls: ['./add-button.component.scss'],
 })
 export class AddButtonComponent {
-  constructor(public shimmerEffect: ShimmerService){}
+  constructor(public shimmerEffect: ShimmerService) {}
   isDropdownOpen = false;
-  displayTable = false ;
+  displayTable = false;
   // showRunButton=false;
-  
+
   @Output() statusTableDisplay = new EventEmitter<boolean>();
   @Output() statusRunButtonDisplay = new EventEmitter<boolean>();
   @Output() emitCardType = new EventEmitter<string>();
@@ -30,7 +30,6 @@ export class AddButtonComponent {
         break;
       case 'Line Chart':
         // Handle line chart button click
-        
         this.toggleDropdown();
         break;
       default:
@@ -46,18 +45,18 @@ export class AddButtonComponent {
       this.isDropdownOpen = true;
     }
   }
-  showTable(){
-
-    of(this.shimmerEffect.shimmering()).pipe(
-      delay(1) // adjust the delay time as needed
-    ).subscribe(() => {
-      this.displayTable = true;
-      this.statusTableDisplay.emit(this.displayTable);
-      this.statusRunButtonDisplay.emit(true);
-      // this.showRunButton = true;
-    });
+  showTable() {
+    of(this.shimmerEffect.shimmering())
+      .pipe(
+        delay(1) // adjust the delay time as needed
+      )
+      .subscribe(() => {
+        this.displayTable = true;
+        this.statusTableDisplay.emit(this.displayTable);
+        this.statusRunButtonDisplay.emit(true);
+        // this.showRunButton = true;
+      });
     // this.displayTable= true;
     // this.statusTableDisplay.emit(this.displayTable)
   }
-
 }
